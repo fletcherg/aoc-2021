5ea4c8159a1ef757b5d8e881e99f0576a8277eb5
Gareth Fletcher <gareth@fletcher.at>
2021-12-05 20:41:08 +1300

    Day5

--- Day 5/Day5.ps1
+++ Day 5/Day5.ps1
@@ -1,21 +1,20 @@
 $puzzle = Get-Content input.txt
 
 ## Part 1
-$points = @(); $puzzle | % {
-    $x1,$y1,$x2,$y2 = $_.replace(" ->","").split(" ").split(",")
-    ($x1 -eq $x2) ? (($y1..$y2) | % {$points += "$([int]$x1),$([int]$_)"} ) : `
-        ( ($y1 -eq $y2) ? (($x1..$x2) | % {$points += "$([int]$_),$([int]$y1)" }) : $null )
+$points = $puzzle | % {
+    $x1,$y1,$x2,$y2 = $_.split(" -> ").split(",")
+    ($x1 -eq $x2) ? (($y1..$y2) | % { "$([int]$x1),$([int]$_)"} ) : `
+        ( ($y1 -eq $y2) ? (($x1..$x2) | % { "$([int]$_),$([int]$y1)" }) : $null )
 }
 ($points | group-object | ? {$_.count -gt 1} | Measure-object).count
 
 ## Part 2
-$points = $puzzle | % -Parallel {
-    $x1,$y1,$x2,$y2 = $_.replace(" ->","").split(" ").split(",")
+$points = $puzzle | %  {
+    $x1,$y1,$x2,$y2 = $_.split(" -> ").split(",")
     ($x1 -eq $x2) ? (($y1..$y2) | % {"$([int]$x1),$([int]$_)"} ) : `
         ( ($y1 -eq $y2) ? (($x1..$x2) | % { "$([int]$_),$([int]$y1)" }) : $( 
             $diag = ($x1..$x2),($y1..$y2)
-            for ($i=0;$i-lt $diag[0].length;$i++) { "$([int]$diag[0][$i]),$([int]$diag[1][$i])" }
+            (0..($diag[0].length - 1)) | % { "$([int]$diag[0][$_]),$([int]$diag[1][$_])" }
          ) )
 }
 ($points | group-object | ? {$_.count -gt 1} | Measure-object).count
-
