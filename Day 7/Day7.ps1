bca767a29992af3e0898439dbcfcbb4eb2e32253
Gareth Fletcher <gareth@fletcher.at>
2021-12-07 20:37:08 +1300

    day7

--- Day 7/Day7.ps1
+++ Day 7/Day7.ps1
@@ -1,19 +1,26 @@
 
-$puzzle = Get-Content sample.txt
+$puzzle = Get-Content input.txt
 $list = $puzzle.split(",") | % { [int]$_ } | sort-object
 
 ## Part 1
 $d = [Int]::MaxValue
 for ($i = ($list | select-object -First 1); $i -le ($list | select-object -Last 1); $i++) {
-    $m = ($list | % { [Math]::Abs($_ - $i) } | Measure-Object -sum).sum
+    $m = ($list | % {
+        [Math]::Abs($_ - $i)
+    } | Measure-Object -sum).sum
     if ($m -lt $d) { $d = $m }
 }
 $d
 
+
 ## Part 2
 $d = [Int]::MaxValue
 for ($i = ($list | select-object -First 1); $i -le ($list | select-object -Last 1); $i++) {
-    $m = ($list | % { ([Math]::Abs($_ - $i) * ([Math]::Abs($_ - $i)+1))/2 } | Measure-Object -sum).sum
-    if ($m -lt $d) { $d = $m }
+    $td = 0
+    $list | % {
+        $td += ([Math]::Abs($_ - $i) * ([Math]::Abs($_ - $i)+1))/2
+        if ($td -gt $d) { break }
+    }
+    if ($td -lt $d) { $d = $td }
 }
 $d
